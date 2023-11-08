6190a0dcb37b010dad0e2fce31f651028f986348
RaulParraUniEibar <75114165+RaulParraUniEibar@users.noreply.github.com>
2023-11-08 12:46:13 +0100

    Perfil Img
    08/11/2023--> 12:46

--- Hozkada/HozkadaBokatak/views.py
+++ Hozkada/HozkadaBokatak/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Platerra
 from .models import Alergia_Platerra
-
+from .models import Bezeroa 
 # Create your views here.
 
 def index(request): 
@@ -11,3 +11,10 @@ def bokatak(request):
     platerra = Platerra.objects.all
     alergia_platerrak = Alergia_Platerra.objects.all
     return render(request, 'bokatak.html', {'bokatak':platerra,'alergia_platerrak':alergia_platerrak})
+
+def imagenperfil(request):
+    # Obtiene el objeto Bezeroa relacionado con el usuario autenticado
+    bezeroa = Bezeroa.objects.get(user=request.user)
+
+    # Pasa el objeto Bezeroa a la plantilla
+    return render(request, 'index.html', {'bezeroa': bezeroa})
