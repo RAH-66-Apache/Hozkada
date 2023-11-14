940658111f37b24a44a71fcb824c2dafcf5abbe2
RaulParraUniEibar <75114165+RaulParraUniEibar@users.noreply.github.com>
2023-11-14 08:21:30 +0100

    get latest version

--- Hozkada/HozkadaBokatak/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class HozkadabokatakConfig(AppConfig):
-    name = 'HozkadaBokatak'
--- turf-master/examples/create-react-app/src/index.js
+++
@@ -1,4 +0,0 @@
-import { greatCircle, point } from "@turf/turf";
-
-console.log(greatCircle([0, 0], [100, 10]));
-console.log(point([100, 0]));
